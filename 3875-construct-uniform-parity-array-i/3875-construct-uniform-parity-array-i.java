85541c61df855b201298301de8cabdd6d4ab2960
pranathi1703 <150605336+pranathi1703@users.noreply.github.com>
2026-09-02 22:42:49 +0530

    Time: 1 ms (31.89%), Space: 45.3 MB (16.73%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,60 @@
 class Solution {
-    public boolean uniformArray(int[] nums1) {
-        return true;
+    public boolean uniformArray(int[] nums) {
+        int n = nums.length;
+        boolean canMakeEven = true;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                continue;
+            }
+            boolean found = false;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    int value = nums[i] - nums[j];
+
+                    if (value % 2 == 0) {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found) {
+                canMakeEven = false;
+                break;
+            }
+        }
+
+        if (canMakeEven) {
+            return true;
+        }
+
+        boolean canMakeOdd = true;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 != 0) {
+                continue;
+            }
+            boolean found = false;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    int value = nums[i] - nums[j];
+
+                    if (value % 2 != 0) {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found) {
+                canMakeOdd = false;
+                break;
+            }
+        }
+
+        return canMakeOdd;
     }
 }
