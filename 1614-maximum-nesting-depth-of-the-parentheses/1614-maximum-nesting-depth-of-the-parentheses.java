b54ab3c2d080a8d53e85b77e89a9a1f13c132c80
pranathi1703 <150605336+pranathi1703@users.noreply.github.com>
2026-09-03 20:01:10 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.32%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxDepth(String s) {
+        int depth = 0, maxDepth = 0;
+        for(int i = 0; i < s.length();i++){
+            if(s.charAt(i) == '('){
+                depth++;
+                maxDepth = Math.max(depth,maxDepth);
+            }else if(s.charAt(i) == ')'){
+                depth--;
+            }
+        }
+        return maxDepth;
+    }
+}
